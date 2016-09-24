ade7057a03cae6b191e5a02bf5cc1e6054f6a275
Natsuki Sugawara <natsuki.sugawara@luxiar.com>
2016-09-24 23:49:52 +0900

    補足

--- www/js/services/match.js
+++ www/js/services/match.js
@@ -13,7 +13,7 @@ app.factory('Match', function(FURL, $firebaseArray, $ionicPopup){
 
       check.on('value', function(snap){
         if (snap.val() != null) {
-          ref.child('matches').child(uid1).child(uid2).set(true);
+          ref.child('matches').child(uid1).child(uid2).set(true); //マッチしたペアをDBに保存
           ref.child('matches').child(uid2).child(uid1).set(true);
 
           $ionicPopup.alert({
@@ -23,8 +23,9 @@ app.factory('Match', function(FURL, $firebaseArray, $ionicPopup){
         }
       })
     },
+    // 交際オファーを拒否した時の処理
     removeMatch: function(uid1, uid2) {
-      ref.child('matches').child(uid1).child(uid2).remove();
+      ref.child('matches').child(uid1).child(uid2).remove(); // DBからマッチしたペアを消去している
       ref.child('matches').child(uid2).child(uid1).remove();
 
     }
